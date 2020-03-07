07058cb2c078338cbb7623f86df96a1deffd30c9
Michael Sell <michael.sell@usa.net>
2020-03-07 18:19:07 +0100

    basics for contact context set up

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,25 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 
+import ContactState from './context/contact/ContactState';
+
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <div className='container'>
-          <Navbar />
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <ContactState>
+      <Router>
+        <Fragment>
+          <div className='container'>
+            <Navbar />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
 };
 
