522fb163f80347a3083b5f2fa01a12eb0481b5e7
Michael Sell <michael.sell@usa.net>
2020-03-07 08:02:24 +0100

    added Middleware to enforce authentication

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const User = require('./../models/User');
@@ -9,8 +10,14 @@ const User = require('./../models/User');
 //@route    GET api/auth
 //@desc     Get logged-in user
 //@access   Private
-router.get('/', (req, res) => {
-  res.send('Get logged-in user.');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 //@route    POST api/auth
