5bc586809873b3bb250c99691c942343f4d30e8d
Michael Sell <michael.sell@usa.net>
2020-03-13 12:14:26 +0100

    added alert, alertContext and alert function to Register

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,10 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
 
 import ContactState from './context/contact/ContactState';
+import AlertState from './context/alert/AlertState';
 import AuthState from './context/auth/AuthState';
 
 import './App.css';
@@ -15,21 +17,24 @@ import './App.css';
 const App = () => {
   return (
     <AuthState>
-      <ContactState>
-        <Router>
-          <Fragment>
-            <div className='container'>
-              <Navbar />
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/register' component={Register} />
-                <Route exact path='/login' component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
-      </ContactState>
+      <AlertState>
+        <ContactState>
+          <Router>
+            <Fragment>
+              <div className='container'>
+                <Navbar />
+                <Alerts />
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </ContactState>
+      </AlertState>
     </AuthState>
   );
 };
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import AlertContext from './../../context/alert/alertContext';
 
 const Register = () => {
+  const alertContext = useContext(AlertContext);
+
+  const { setAlert } = alertContext;
+
   const [user, setUser] = useState({
     name: '',
     email: '',
@@ -15,8 +20,16 @@ const Register = () => {
       ...user,
       [e.target.name]: e.target.value
     });
+
   const onSubmit = e => {
-    e.prevent.Default();
+    e.preventDefault();
+    if (name === '' || email === '' || password === '') {
+      setAlert('Please add all information', 'danger');
+    } else if (password !== password2) {
+      setAlert('Passwords do not match!', 'danger');
+    } else {
+      console.log('Error in Register form');
+    }
   };
 
   return (
@@ -27,11 +40,23 @@ const Register = () => {
       <form onSubmit={onSubmit}>
         <div className='form-group'>
           <label htmlFor='name'>Name</label>
-          <input type='text' name='name' value={name} onChange={onChange} />
+          <input
+            type='text'
+            name='name'
+            value={name}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='email'>Email Address</label>
-          <input type='email' name='email' value={email} onChange={onChange} />
+          <input
+            type='email'
+            name='email'
+            value={email}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='password'>Password</label>
@@ -40,6 +65,8 @@ const Register = () => {
             name='password'
             value={password}
             onChange={onChange}
+            required
+            minLength='6'
           />
         </div>
         <div className='form-group'>
@@ -49,6 +76,8 @@ const Register = () => {
             name='password2'
             value={password2}
             onChange={onChange}
+            required
+            minLength='6'
           />
         </div>
         <input
