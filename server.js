102bbf3a719cc2435f78eb40a26563ca8fc65806
Michael Sell <michael.sell@usa.net>
2020-03-17 07:56:22 +0100

    Prepared for eplayment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db.js');
+const path = require('path');
 
 const app = express();
 connectDB();
@@ -8,12 +9,22 @@ app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the ContactKeeper API ...' })
-);
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+/* app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the ContactKeeper API ...' })
+); */
+
+// Serve static assets in production
+if (process.envNODE_ENV === 'production') {
+  //set the static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
